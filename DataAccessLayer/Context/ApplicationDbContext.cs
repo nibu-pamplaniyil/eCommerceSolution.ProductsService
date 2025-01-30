04fc6ad04548193f5cc9598425a04ce10cf28b05
nibu-pamplaniyil <nibupamplaniyil@gmail.com>
2025-01-30 20:53:58 +0530

    Database creation & Connection

---
+++ DataAccessLayer/Context/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using DataAccessLayer.Entites;
+using Microsoft.EntityFrameworkCore;
+
+namespace eCommerce.DataAccessLayer.Context;
+public class ApplicationDbContext : DbContext 
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+
+    }
+
+    public DbSet<Product> Products { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
