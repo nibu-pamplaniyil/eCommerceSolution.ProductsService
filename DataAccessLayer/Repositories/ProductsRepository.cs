5ac3e7c6c2c8b2f6a6b48862376f673cc7dabba3
nibu-pamplaniyil <nibupamplaniyil@gmail.com>
2025-02-01 20:46:34 +0530

    Repositories are created

---
+++ DataAccessLayer/Repositories/ProductsRepository.cs
@@ -0,0 +1,68 @@
+﻿
+
+using DataAccessLayer.Entites;
+using eCommerce.DataAccessLayer.Context;
+using eCommerce.DataAccessLayer.RepositoryContracts;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace eCommerce.DataAccessLayer.Repositories;
+public class ProductsRepository : IProductsRepository
+{
+    private readonly ApplicationDbContext _dbContext;
+
+    public ProductsRepository(ApplicationDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+    public async Task<Product?> AddProduct(Product product)
+    {
+        _dbContext.Products.Add(product);
+        await _dbContext.SaveChangesAsync();
+        return product;
+    }
+
+    public async Task<bool> DeleteProduct(Guid productID)
+    {
+        Product? existingProduct = await _dbContext.Products.FirstOrDefaultAsync(x => x.ProductID == productID);
+        if (existingProduct == null)
+        {
+            return false;
+        }
+        _dbContext.Products.Remove(existingProduct);
+        int affectedRowsCount = await _dbContext.SaveChangesAsync();
+        return affectedRowsCount>0;
+    }
+
+    public async Task<Product?> GetProductByCondtion(Expression<Func<Product, bool>> conditionExpression)
+    {
+        return await _dbContext.Products.FirstOrDefaultAsync(conditionExpression);
+    }
+
+    public async Task<IEnumerable<Product>> GetProducts()
+    {
+        return await _dbContext.Products.ToListAsync();
+    }
+
+    public async Task<IEnumerable<Product?>> GetProductsByCondition(Expression<Func<Product, bool>> conditionExpression)
+    {
+        return await _dbContext.Products.Where(conditionExpression).ToListAsync();
+    }
+
+    public async Task<Product?> UpdateProduct(Product product)
+    {
+        Product? exixtingProduct = await _dbContext.Products.FirstOrDefaultAsync(x=>x.ProductID == product.ProductID);
+        if(exixtingProduct == null) 
+        {
+           return null; 
+        }
+        exixtingProduct.ProductName = product.ProductName;
+        exixtingProduct.UnitPrice = product.UnitPrice;
+        exixtingProduct.QuantityInStock = product.QuantityInStock;
+        exixtingProduct.Category = product.Category;
+
+        await _dbContext.SaveChangesAsync();
+        return exixtingProduct;
+    }
+}
---
+++ DataAccessLayer/RepositoryContracts/IProductsRepository.cs
@@ -0,0 +1,22 @@
+﻿
+
+using DataAccessLayer.Entites;
+using System.Linq.Expressions;
+
+namespace eCommerce.DataAccessLayer.RepositoryContracts;
+public interface IProductsRepository
+{
+    //Retrieves all products asynchronously
+    Task<IEnumerable<Product>> GetProducts();
+
+    //Retrieves all products based on condition asynchronously
+    Task<IEnumerable<Product?>> GetProductsByCondition(Expression<Func<Product, bool>> conditionExpression);
+
+    Task<Product?> GetProductByCondtion(Expression<Func<Product, bool>> conditionExpression);
+
+    //Adding new item to product table asynchronously
+    Task<Product?> AddProduct(Product product);
+
+    Task<Product?> UpdateProduct(Product product); 
+    Task<bool> DeleteProduct(Guid productID);
+}
